d2c7d35a45db16f902933c5dfe685bbce893a63e
Mel6 <MelVelezE@gmail.com>
2016-11-18 20:12:40 -0500

    PickUpObject & Throw
    This is the updated version of the PickUpObject code. This is for the player to press the "E" key to pick up the object and again to throw it.

--- Different-Object-Interaction-Codes/PickUpObject.cs
+++ Different-Object-Interaction-Codes/PickUpObject.cs
@@ -5,9 +5,9 @@ public class PickUpObject : MonoBehaviour {
 
 	public bool IsGrabbed;
 	RaycastHit2D hit;
-	public float distance=1f;
+	private float distance = 1f;
 	public Transform grabobject;
-	public float throwforce;
+	private float Throwforce = 10;
 	public LayerMask Notgrabbed;
 
 	// Use this for initialization
@@ -18,38 +18,35 @@ public class PickUpObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.E))
-		{
-			if(!IsGrabbed)
-			{
-				Physics2D.queriesStartInColliders=false;
-				Debug.Log ("Is there a Collider?");
-				hit =	Physics2D.Raycast(transform.position,Vector2.right*transform.localScale.x,distance);
-				if(hit.collider!=null && hit.collider.tag=="grab")
+		if (Input.GetKeyDown (KeyCode.E)) {
+			if (!IsGrabbed) {
+				Physics2D.queriesStartInColliders = false;
+				hit = Physics2D.Raycast (transform.position, Vector2.right * transform.localScale.x, distance);
+				if (hit.collider != null && hit.collider.tag == "grab") 
 				{
-					IsGrabbed=true;
+					IsGrabbed = true;
 				}
-					
-				//Grab the item
-			}else if(!Physics2D.OverlapPoint(grabobject.position,Notgrabbed))
+				//GRAB THE ITEM
+				//IF AN EMPTY OBJECT IS CREATED AS USED AS A CHILD, MAKE SURE "X" IS 1.7, SO THE PLAYER CAN GRAB THE OBJECT WITHOUT IT SLIDING AWAY
 
-			{
-				IsGrabbed=false;
-				if(hit.collider.gameObject.GetComponent<Rigidbody2D>()!=null)
-				{
-					hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity= new Vector2(transform.localScale.x,1)*throwforce;
+			} else if (!Physics2D.OverlapPoint (grabobject.position, Notgrabbed)) {
+				IsGrabbed = false;
+
+				if (hit.collider.gameObject.GetComponent<Rigidbody2D> () != null) {
+					hit.collider.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x, 1) * Throwforce;
 				}
-				//Throw the item
+				//THROW THE ITEM
 			}
 		}
 
-		if (IsGrabbed)
+		if (IsGrabbed) {
 			hit.collider.gameObject.transform.position = grabobject.position;
+		}
 	}
 
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.green;
-		Gizmos.DrawLine(transform.position,transform.position+Vector3.right*transform.localScale.x*distance);
+		Gizmos.DrawLine(transform.position,transform.position+Vector3.right*transform.localScale.x *distance);
 	}
 }
