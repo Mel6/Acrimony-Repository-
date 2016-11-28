eee067336c2556ef5043c24cfc490cc4abeb0bd9
Mel6 <MelVelezE@gmail.com>
2016-11-28 15:03:06 -0500

    PlayerControl Script
    *Updated as I finally got the character to jump once. The issue was to move the GroundCheck file far below the player's feet.

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
@@ -7,11 +7,20 @@ public class PlayerControl : MonoBehaviour {
 	public int jumpSpeed;
 	public int gravity;
 
-	Rigidbody2D rb2D;
+	public Transform grounder;
+	public float radius;
+	public LayerMask ground;
+	public bool isGrounded = false;
+
+	private Rigidbody2D rb2D;
 
 	// Use this for initialization
 	void Start () {
 		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate (){
+		isGrounded = Physics2D.OverlapCircle (grounder.position, radius, ground);
 	}
 	
 	// Update is called once per frame
@@ -19,13 +28,13 @@ public class PlayerControl : MonoBehaviour {
 		Vector2 moveDirection = new Vector2 (Input.GetAxisRaw("Horizontal") * 4, rb2D.velocity.y); //5 is for the speed
 		rb2D.velocity = moveDirection;
 
-		if (Input.GetAxisRaw("Horizontal") == 1){
+		if (Input.GetAxisRaw("Horizontal") == 1){						//Moves Right
 			transform.localScale = new Vector3(1, 1, 1);
-		} else if (Input.GetAxisRaw("Horizontal") == -1){
+		} else if (Input.GetAxisRaw("Horizontal") == -1){				//Moves Left
 			transform.localScale = new Vector3(-1, 1, 1);
 		}
 
-		if (Input.GetKeyDown(KeyCode.Space)){
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded==true){				//For Jumping
 			rb2D.AddForce (new Vector2 (0, 270)); //270 is for the jumpSpeed
 		}
 	}
